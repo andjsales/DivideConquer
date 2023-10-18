4218bc0ea833610a0a124e74983ce35d6c25f3f6
andrew.sales <andjsales@gmail.com>
2023-10-18 16:07:50 -0500

    fixed mistakes

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,7 +1,11 @@
 function findRotatedIndex(arr, num) {
     let pivot = findPivot(arr);
     if (pivot > 0 && num >= arr[0] && num <= arr[pivot - 1]) {
+        let start = 0;
+        let end = pivot - 1;
+        return binarySearch(arr, num, start, end); // checking left side of array
     }
+    return binarySearch(arr, num, pivot, arr.length - 1); // checking right side of array
 }
 
 function binarySearch(arr, num, start, end) {
@@ -24,15 +28,25 @@ function binarySearch(arr, num, start, end) {
         }
     }
     return -1;
+
 }
 
 function findPivot(arr) {
-    if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
+    // [3, 4, 1, 2]
+    // finding where the array pivots
+
+    // 
+    if (arr.length === 1 || arr[0] < arr[arr.length - 1]) { // 3 < 2
+        return 0;
+    }
+
     let start = 0;
-    let end = arr.length - 1;
+    let end = arr.length - 1; // 3
     while (start <= end) {
-        let middle = Math.floor((start + end) / 2);
-        if (arr[middle] > arr[middle + 1]) return middle + 1;
+        let middle = Math.floor((start + end) / 2); // 1
+        if (arr[middle] > arr[middle + 1]) { // 4 > 1
+            return middle + 1; // 2
+        }
         else if (arr[start] <= arr[middle]) {
             start = middle + 1;
         } else {
@@ -41,5 +55,8 @@ function findPivot(arr) {
     }
 }
 
+// expect(findRotatedIndex([3, 4, 1, 2], 4)).toBe(1);
+const result = findRotatedIndex([3, 4, 1, 2], 4);
+console.log(result);
 
 module.exports = findRotatedIndex;
